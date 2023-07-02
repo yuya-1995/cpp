2299e2dfa86d5e35a9528d7a0ad87f78bbec57cb
yuya-1995 <sasa19951018@gmail.com>
2023-07-02 22:49:49 +0900

    lesson4　練習問題

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,6 +4,47 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	/*double height;
+	double line;
+	int rule = 2;
+
+	cout << "三角形の高さを入力してください\n";
+	cin >> height;
+	cout << "三角形の底辺を入力してください\n";
+	cin >> line;
+	cout << "三角形の面積は" << line * height / rule << "です\n";*/
+
+	int num1, num2, num3, num4, num5;
+	int total;
+	double ave;
+	for (int i = 1; i <=  5; i++)
+	{
+		cout << "科目" << i << "の点数を入力してください\n";
+		if (i == 1)
+		{
+			cin >> num1;
+		}else if(i == 2)
+		{
+			cin >> num2;
+		}
+		else if (i == 3)
+		{
+			cin >> num3;
+		}
+		else if (i == 4)
+		{
+			cin >> num4;
+		}
+		else if (i == 5)
+		{
+			cin >> num5;
+			total = num1 + num2 + num3 + num4 + num5;
+			ave = (double)total / i;
+			cout << "合計点" << total << "\n";
+			cout << "平均点" << ave << "\n";
+
+		}
+	}
+	
 }
 
